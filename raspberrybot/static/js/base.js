473b67fa0265c49617605d26972c2c31dfef065c
qoda <jonathan.bydendyk@is.co.za>
2014-05-25 18:04:50 +0200

    minor bugfixes

--- raspberrybot/static/js/base.js
+++ raspberrybot/static/js/base.js
@@ -1,6 +1,7 @@
 initKeyboardControls = function () {
 
     // map key code to command verb
+    var current_command = null;
     var command_map = {
         37: 'left',
         38: 'forward',
@@ -9,13 +10,24 @@ initKeyboardControls = function () {
 
     // execute command depending on the key pressed
     $(document).keydown(function(event){
-        $.get("/control/" + command_map[event.keyCode] + "/");
+        var event_command = command_map[event.keyCode];
+        if ((event_command != undefined) && (current_command != event_command)) {
+            $.get("/control/" + event_command + "/");
+            current_command = event_command;
+        }
+
     });
 
     // send stop command on keyup
     $(document).keyup(function(event){
-        $.get("/control/stop/");
+        if (current_command != 'stop') {
+            $.get("/control/stop/");
+            current_command = 'stop';
+        }
     });
+
+    // log the command to console
+    console.log(current_command);
 }
 
 $(document).ready( function () {
