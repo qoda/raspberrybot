9bc043284c9b114817355b55f7bc23526dd3f271
qoda <jonathan.bydendyk@is.co.za>
2014-05-25 20:19:00 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # project description
     name='raspberrybot',
-    version='0.1.3',
+    version='0.1.4',
     description='A Simple WiFi Controlled RaspberryPi Robot',
     long_description="%s\n\n%s" % (open('README.rst', 'r').read(), open('AUTHORS.rst', 'r').read()),
     author='Jonathan Bydendyk',
