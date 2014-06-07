18ac0301017995bf8ef43b1d052955c60b99a701
qoda <jonathan.bydendyk@is.co.za>
2014-06-07 23:40:08 +0200

    improved server script

--- server.sh
+++ server.sh
@@ -1,3 +1,7 @@
-mjpg_streamer -i "/usr/lib/input_uvc.so -y -d /dev/video0" -o "/usr/lib/output_http.so -p 8090"
+mjpg_streamer -i "/usr/lib/input_uvc.so -y -d /dev/video0" -o "/usr/lib/output_http.so -p 8090" &
+ms_pid=$!
+
 export DJANGO_SETTINGS_MODULE='raspberrybot.settings'
-./bin/django-admin.py runserver 10.0.0.7:8000
+./bin/django-admin.py runserver 0.0.0.0:8000
+
+kill $ms_pid 2>/dev/null
