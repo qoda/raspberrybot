ca50f3db9020bba222b67edb79cdd64be777a214
qoda <jonathan.bydendyk@is.co.za>
2014-06-08 16:23:41 +0200

    added videostream to server startup script

--- server.sh
+++ server.sh
@@ -1,7 +1,6 @@
-mjpg_streamer -i "input_uvc.so -y -q 95 -f 15 -d /dev/video0" -o "output_http.so -p 8090" &
-ms_pid=$!
-
 export DJANGO_SETTINGS_MODULE='raspberrybot.settings'
+./bin/django-admin.py videoserver 0.0.0.0:8090 &
+videoserver_pid=$!
 ./bin/django-admin.py runserver 0.0.0.0:8000
 
-kill $ms_pid 2>/dev/null
+kill $videoserver_pid 2>/dev/null
