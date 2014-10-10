7f5dff4fb344cebb6e680473c435d40ffa278032
qoda <jpbydendyk@gmail.com>
2014-10-10 23:52:05 +0200

    Added facial recognition

--- raspberrybot/apps/vision/management/commands/videoserver.py
+++ raspberrybot/apps/vision/management/commands/videoserver.py
@@ -1,8 +1,10 @@
+import os
 import time
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from SimpleCV import Camera, JpegStreamer
+from SimpleCV import Camera, Color, DrawingLayer, JpegStreamer
 
 
 class Command(BaseCommand):
@@ -12,17 +14,39 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         host = options.get('host', '0.0.0.0')
         port = options.get('port', '8090')
-        width = options.get('width', 640)
-        height = options.get('height', 480)
-        quality = options.get('quality', 70)
         host_camera = options.get('host_camera', 0)
 
         # setup the stream
-        camera = Camera(host_camera, {"width": width, "height": height})
+        camera = Camera(host_camera, {"width": 800, "height": 600})
         stream = JpegStreamer("%s:%s" % (host, port))
 
         while True:
-            camera.getImage().save(stream, quality=quality)
+            detect_faces = os.path.exists(
+                os.path.join(settings.BASE_DIR, '.detect')
+            )
+            full_image = camera.getImage()
+            if detect_faces:
+                small_image = full_image.scale(200, 150)
+                facial_features = small_image.findHaarFeatures('face')
+                if facial_features is not None:
+                    x = facial_features.x() * 4
+                    y = facial_features.y() * 4
+                    width = facial_features.width() * 4
+                    height = facial_features.height() * 4
+
+                    face_layer = DrawingLayer(
+                        (full_image.width, full_image.height)
+                    )
+                    face_layer.centeredRectangle(
+                        (x[0], y[0]),
+                        (width[0], height[0]),
+                        color=Color.ORANGE,
+                        width=1
+                    )
+                    full_image.addDrawingLayer(face_layer)
+                    full_image.applyLayers()
+
+            full_image.save(stream, quality=60)
 
             # ensure it sleeps for as long as the fps in this case 10 fps
             time.sleep(0.1)
