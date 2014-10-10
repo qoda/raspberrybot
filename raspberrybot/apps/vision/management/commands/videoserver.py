59549faa244d17d06d0e3c657b6426dfa5c310fd
qoda <jpbydendyk@gmail.com>
2014-10-10 21:18:12 +0200

    Optimised video streaming

--- raspberrybot/apps/vision/management/commands/videoserver.py
+++ raspberrybot/apps/vision/management/commands/videoserver.py
@@ -12,15 +12,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         host = options.get('host', '0.0.0.0')
         port = options.get('port', '8090')
+        width = options.get('width', 640)
+        height = options.get('height', 480)
+        quality = options.get('quality', 70)
         host_camera = options.get('host_camera', 0)
 
         # setup the stream
-        camera = Camera(host_camera)
+        camera = Camera(host_camera, {"width": width, "height": height})
         stream = JpegStreamer("%s:%s" % (host, port))
 
         while True:
-            image = camera.getImage()
-            image.save(stream)
+            camera.getImage().save(stream, quality=quality)
 
             # ensure it sleeps for as long as the fps in this case 10 fps
             time.sleep(0.1)
