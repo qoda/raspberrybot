2affb107b865ee20e66412717b3940e0d5d0fe26
qoda <jpbydendyk@gmail.com>
2014-10-17 14:04:27 +0200

    Fixed server script

--- server.sh
+++ server.sh
@@ -1,8 +1,9 @@
-export DJANGO_SETTINGS_MODULE='raspberrybot.settings'
 export LD_PRELOAD=/usr/lib/uv4l/uv4lext/armv6l/libuv4lext.so
 
-./bin/django-admin.py videoserver 0.0.0.0:8090 &
+. bin/activate
+
+./manage.py videoserver 0.0.0.0:8090 &
 videoserver_pid=$!
-./bin/django-admin.py runserver 0.0.0.0:8000
+./manage.py runserver 0.0.0.0:8000
 
 kill $videoserver_pid 2>/dev/null
